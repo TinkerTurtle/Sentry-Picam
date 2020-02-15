fe348f148384cbee03243bf3421dac034e57ed24
TinkerTurtle <tinkerturtle@ubercharge.me>
2020-02-15 18:12:12 -0500

    fix recorder

--- pkg/raspivid/recorder.go
+++ pkg/raspivid/recorder.go
@@ -2,6 +2,7 @@ package raspivid
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"simple-webcam/broker"
 	"time"
@@ -13,14 +14,18 @@ type Recorder struct {
 	StopTime        time.Time
 }
 
+func getFilename() string {
+	fileFormat := "%d-%02d-%02d-%02d00.h264"
+	now := time.Now()
+	return fmt.Sprintf(fileFormat, now.Year(), now.Month(), now.Day(), now.Hour())
+}
+
 // Init initializes the raspivid recorder. folderpath must include the trailing slash
 func (rec *Recorder) Init(caster *broker.Broker, folderpath string) {
 	stream := caster.Subscribe()
 	numHeaders := 0
 
-	fileFormat := "%d-%02d-%02d-%02d00.h264"
-	now := time.Now()
-	fileName := fmt.Sprintf(fileFormat, now.Year(), now.Month(), now.Day(), now.Hour())
+	fileName := getFilename()
 
 	f, _ := os.Create(folderpath + fileName)
 	defer f.Close()
@@ -31,17 +36,19 @@ func (rec *Recorder) Init(caster *broker.Broker, folderpath string) {
 		x := <-stream
 
 		if rec.RequestedRecord && time.Now().Before(rec.StopTime) {
-			now = time.Now()
-			newName := fmt.Sprintf(fileFormat, now.Year(), now.Month(), now.Day(), now.Hour())
+			newName := getFilename()
 			if fileName != newName {
 				fileName = newName
 				f.Close()
-				f, _ := os.Create(folderpath + fileName)
+				f, _ = os.Create(folderpath + fileName)
 				defer f.Close()
 			}
 
 			for _, v := range buf {
-				f.Write(v)
+				_, err := f.Write(v)
+				if err != nil {
+					log.Println(err)
+				}
 			}
 			buf = buf[:0]
 			numHeaders = 0
